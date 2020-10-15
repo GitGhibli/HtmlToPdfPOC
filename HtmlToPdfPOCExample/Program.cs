92e2e262b7f190c6fbfb03a7542197865b4a2916
Martin Galajda <martin@yarmill.com>
2020-10-15 14:41:56 +0200

    initial commit

---
+++ HtmlToPdfPOCExample/Program.cs
@@ -0,0 +1,31 @@
+﻿using DinkToPdf;
+
+namespace DinkToPdf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var doc = new HtmlToPdfDocument()
+            {
+                GlobalSettings = 
+                {
+                    ColorMode = ColorMode.Color,
+                    Orientation = Orientation.Landscape,
+                    PaperSize = PaperKind.A4Plus,
+                    Out = @"D:\Temp\Yarmill.pdf",
+                },
+                Objects = {
+                    new ObjectSettings()
+                    {
+                        Page = "https://www.whatsmybrowser.org/"
+                        // Page = "https://ywa-740-browser-check--friendly-hermann-eceb82.netlify.app/plan/week?group=93&week=2020-09-29&avoidBrowserCheck=1",
+                    }
+                }
+            };
+
+            var converter = new BasicConverter(new PdfTools());
+            converter.Convert(doc);
+        }
+    }
+}
---
+++ WkHtmlToPdfNetCoreExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WkHtmlToPdf.Net.Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
